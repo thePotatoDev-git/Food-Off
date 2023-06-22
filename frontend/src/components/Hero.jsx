3dc6a71ee27ee3984a2d7bd64b226200891d0f21
Albertus Chua <albertus.chua@gmail.com>
2023-06-22 17:42:44 -0400

    Added carousel feature for Hero page

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleLeft, faCircleRight } from '@fortawesome/free-solid-svg-icons';
 import { useState, useEffect } from 'react';
@@ -17,17 +18,49 @@ const Hero = () => {
         }
     }, [navigate, userInfo]);
 
+    // Hero carousel
+    const carouselPages = [
+        {
+            img: "../images/pic01.jpg",
+            desc: 'Having trouble deciding where to eat? Food/Off can help you decide!'
+        },
+        {
+            img: "../images/pic02.jpg",
+            desc: "Eateries face off against each other and the last choice standing is what's for dinner!"
+        },
+        {
+            img: "../images/pic03.jpg",
+            desc: "Add eateries to create your own personal list of your favorite places to eat."
+        },
+    ];
+    
+
+    const [page, setPage] = React.useState(0);
+
+    function nextPage() {
+        if (page < carouselPages.length - 1) {
+            setPage(page + 1);
+        }
+    }
+
+    function prevPage() {
+        if (page > 0) {
+            setPage(page - 1);
+        }
+    }
+    
+
     return (
         <>
             <section className="main">
                 <h1 className="hero--header">Food/Off</h1>
-                <img className="hero--img" src="../images/pic01.jpg" alt="" />
+                <img className="hero--img" src={carouselPages[page].img} alt="" />
                 <p className="hero--desc">
-                    Having trouble deciding where to eat? Food/Off can help you decide!
+                    {carouselPages[page].desc}
                 </p>
                 <div className="arrows">
-                    <FontAwesomeIcon icon={faCircleLeft} size="2xl"/>
-                    <FontAwesomeIcon icon={faCircleRight} size="2xl"/>
+                    <FontAwesomeIcon icon={faCircleLeft} size="2xl" onClick={prevPage}/>
+                    <FontAwesomeIcon icon={faCircleRight} size="2xl" onClick={nextPage}/>
                 </div>
             </section>
             <div className="hero--buttons">
