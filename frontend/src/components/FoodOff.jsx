a95717681f03f134cfa42f148720b096d7336334
Albertus Chua <albertus.chua@gmail.com>
2023-08-31 17:21:17 -0400

    Fixed error that occurred when foodOptions array was empty

--- frontend/src/components/FoodOff.jsx
+++ frontend/src/components/FoodOff.jsx
@@ -6,17 +6,24 @@ export default function FoodOff(props) {
         <>
             <section className="main">
                 <h3>Ready, set, FOOD/OFF!</h3>
-                <section className="food-off">
-                    <div className="option" onClick={props.optionOne}>
-                        <span>{props.foodOptions[0].eateryName}</span>
-                    </div>
-                    <h1>VS</h1>
-                    <div className="option" onClick={props.optionTwo}>
-                        <span>{props.foodOptions[props.foodOptions.length - 1].eateryName}</span>
-                    </div>
+                {
+                    props.foodOptions.length > 0 ? (
+                        <section className="food-off">
+                            <div className="option" onClick={props.optionOne}>
+                                <span>{props.foodOptions[0].eateryName}</span>
+                            </div>
+                            <h1>VS</h1>
+                            <div className="option" onClick={props.optionTwo}>
+                                <span>{props.foodOptions[props.foodOptions.length - 1].eateryName}</span>
+                            </div>
+                        </section>
+                    ) : (
+                        <section className="food-off">
+                            <p>No eateries in this budget range!</p>
+                        </section>
+                    )
+                }
                     <button className="lrg" onClick={() => props.togglePage('budget-menu')}>Start over?</button>
-                </section>
-                <button onClick={() => props.togglePage('winner')}>Winner page test</button>
             </section>
         </>
     )
